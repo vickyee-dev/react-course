2288db762fc97ae025c504619fdf13abd809991a
vickyee-dev <victorpeter0540@gamil.com>
2024-11-18 22:12:48 +0300

    background changer

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('olive')
+
+  // function changeColor(color) {
+  //   setColor(color)
+  // }
+
+  return (
+    <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+      <div className='fixed flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button
+          onClick={() => setColor('red')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: 'red'}}
+          >red</button>
+          <button
+          onClick={() => setColor('green')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: 'green'}}
+          >green</button>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
